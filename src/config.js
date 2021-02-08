87c3c32b6869b10efe5935bf4eb288e1a04c4f61
GooGrand <55867268+GooGrand@users.noreply.github.com>
2021-02-09 00:16:29 +0600

    reworked saving system

---
+++ src/config.js
@@ -0,0 +1,15 @@
+const headers = {
+        'Accept': 'application/json',
+        'X-API-Key': 'superb',
+        'Content-Type': 'application/json'
+    };
+const nodeUrl  = "http://127.0.0.1:6869"
+const nodeTestnetUrl = 'https://nodes-testnet.wavesnodes.com';
+const distributorSeed = 'liberty toast flower vivid park gain sketch eagle image develop april still pumpkin swap sniff';
+
+module.exports = {
+    distributorSeed,
+    headers,
+    nodeTestnetUrl,
+    nodeUrl
+}
